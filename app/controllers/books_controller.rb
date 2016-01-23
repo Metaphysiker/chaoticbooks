50f517b39dcf99cc49dffbd2c9d5e7751b9a387c
Metaphysiker <s.raess@me.com>
2016-01-23 20:21:38 +0100

    added controller codes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update]
 
   def index
-
+    @books = Book.all
   end
 
   def show
@@ -12,7 +13,18 @@ class BooksController < ApplicationController
 
   end
 
+  def new
+    @book = Book.new
+  end
+
   def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
 
   end
 
@@ -21,7 +33,21 @@ class BooksController < ApplicationController
   end
 
   def update
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
 
+  private
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:booktitle, :bookdescription)
   end
 
 end
